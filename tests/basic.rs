e24f124292d1ec9dce155f9837dcdf5bff97d48d
Ivan Dubrov <dubrov.ivan@gmail.com>
2022-12-09 20:41:58 -0600

    Reorganize tests and do some cleanup
    Also, remove `from_value` function as `serde_json::from_value` can be
    used instead (breaking change).

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,28 +1,19 @@
-#![allow(unused)]
-extern crate rand;
-
-use serde_json::json;
-
-mod util;
-
-use super::*;
-use serde_json::from_str;
+use json_patch::{AddOperation, Patch, PatchOperation, RemoveOperation};
+use serde_json::{from_str, from_value, json, Value};
 
 #[test]
 fn parse_from_value() {
-    use PatchOperation::*;
-
     let json = json!([{"op": "add", "path": "/a/b", "value": 1}, {"op": "remove", "path": "/c"}]);
     let patch: Patch = from_value(json).unwrap();
 
     assert_eq!(
         patch,
         Patch(vec![
-            Add(AddOperation {
+            PatchOperation::Add(AddOperation {
                 path: String::from("/a/b"),
                 value: Value::from(1),
             }),
-            Remove(RemoveOperation {
+            PatchOperation::Remove(RemoveOperation {
                 path: String::from("/c"),
             }),
         ])
@@ -35,8 +26,6 @@ fn parse_from_value() {
 
 #[test]
 fn parse_from_string() {
-    use PatchOperation::*;
-
     let patch: Patch =
         from_str(r#"[{"op": "add", "path": "/a/b", "value": 1}, {"op": "remove", "path": "/c"}]"#)
             .unwrap();
@@ -44,11 +33,11 @@ fn parse_from_string() {
     assert_eq!(
         patch,
         Patch(vec![
-            Add(AddOperation {
+            PatchOperation::Add(AddOperation {
                 path: String::from("/a/b"),
                 value: Value::from(1),
             }),
-            Remove(RemoveOperation {
+            PatchOperation::Remove(RemoveOperation {
                 path: String::from("/c"),
             }),
         ])
@@ -62,24 +51,4 @@ fn serialize_patch() {
 
     let serialized = serde_json::to_string(&patch).unwrap();
     assert_eq!(serialized, s);
-}
-
-#[test]
-fn tests() {
-    util::run_specs("specs/tests.json");
-}
-
-#[test]
-fn spec_tests() {
-    util::run_specs("specs/spec_tests.json");
-}
-
-#[test]
-fn revert_tests() {
-    util::run_specs("specs/revert_tests.json");
-}
-
-#[test]
-fn merge_tests() {
-    util::run_specs("specs/merge_tests.json");
 }
